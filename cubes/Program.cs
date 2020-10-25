a1c737db520d5316a2e6b8b516e254014dd02985
Xavier Sanchez <xaviersanchez@dorna.com>
2020-10-25 18:30:45 +0100

    initial version

---
+++ cubes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cubes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
